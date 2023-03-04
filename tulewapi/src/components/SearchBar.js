028092a55e889ce031ba95b80bff39641b8381e5
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2023-03-04 10:13:10 +0300

    added search functionality

--- tulewapi/src/components/SearchBar.js
+++ tulewapi/src/components/SearchBar.js
@@ -1,12 +1,26 @@
-import {React} from 'react';
+import {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SearchBar(){
+
+    const [searchTerm, setSearchTerm] = useState('');
+    let navigate = useNavigate();
+
+     const handleSubmit = (e) => {
+          e.preventDefault();
+          if (searchTerm.length > 0) {
+            localStorage.setItem('searchTerm', searchTerm);
+            navigate('/SearchResults');
+          } else {
+            alert('Input something to search');
+          }
+        };
 
  return(
     <>
         <div className="SearchBar">
-            <form>
-                <input className="mini-bar" type="tetx" placeholder="Search"/>
+            <form onSubmit={handleSubmit}>
+                <input className="mini-bar" type="tetx" placeholder="Search" onChange={(e)=>setSearchTerm(e.target.value)}/>
                 <button className="mini-btn"><i className="fa fa-search"></i></button>
             </form>
         </div>
